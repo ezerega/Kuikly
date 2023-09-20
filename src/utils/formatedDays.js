18ea7a4a3a5b74c180a9d0a6849e5dae7014fc25
ezerega <ezerega@gmail.com>
2023-09-19 18:22:11 -0600

    Update formatedDays.js

--- src/utils/formatedDays.js
+++ src/utils/formatedDays.js
@@ -1,9 +1,10 @@
 import moment from "moment";
 
 //export const currentDate = moment().format("YYYY/MM/DD HH:mm");
-export const currentDate = moment().format("YYYY-MM-DDTHH:mm:ssZ");
-export const nextday = moment(currentDate).add(1, "days").format("YYYY/MM/DD");
-
+//export const currentDate = moment().format("YYYY-MM-DDTHH:mm:ssZ");
+export const currentDate = moment().format("DD/MM/YYYY HH:mm");
+//export const nextday = moment(currentDate).add(1, "days").format("YYYY/MM/DD");
+export const nextday = moment(currentDate).add(1, "days").format("DD/MM/YYYY");
 export const today = moment(currentDate).format("dddd");
 export const tomorrow = moment(nextday).format("dddd");
 
